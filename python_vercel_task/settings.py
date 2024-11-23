d61dc3e707ca441529acc4f68fc8a6a1485b8496
NoniaAcosta <noniaacosta97@gmail.com>
2024-11-23 16:09:43 -0300

    act

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import  config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,6 @@ SECRET_KEY = config('SECRET_KEY_env', default='django-insecure-tlv)36c^h%me#0j)!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-
 
 # Application definition
 
